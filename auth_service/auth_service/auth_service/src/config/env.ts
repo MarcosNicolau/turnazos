688d61811da532b3728fd8f7319d72c400ed3682
nicolau <marcosnicolau2003@gmail.com>
2022-05-19 22:25:47 -0300

    feat: ✨ amqp (RABBIT MQ) configuration
    added amqp with RabbitMQ setup

--- auth_service/auth_service/auth_service/src/config/env.ts
+++ auth_service/auth_service/auth_service/src/config/env.ts
@@ -7,4 +7,6 @@ export const ENV_VARS = {
 	REDIS_HOST: process.env.REDIS_HOST,
 	REDIS_PORT: process.env.REDIS_PORT,
 	REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+	RABBIT_MQ_URL: process.env.RABBIT_MQ_URL,
+	NODE_ENV: process.env.NODE_ENV,
 } as const;
