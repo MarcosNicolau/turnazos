0f1d67207996e84c38c332ce80c22af47e0824f7
nicolau <marcosnicolau2003@gmail.com>
2023-01-31 18:38:15 -0300

    feat(config): ✨ ⚙️ winston logger added file transports by log level
    winston logger added file transports by log level to have an easier separation and debugging

--- logger_service/logger_service/logger_service/logger_service/logger_service/logger_service/src/config/logger.ts
+++ logger_service/logger_service/logger_service/logger_service/logger_service/logger_service/src/config/logger.ts
@@ -1,20 +1,63 @@
 import { ENV_VARS } from "config/env";
+import { loggerLevels, LoggerLevels } from "type/logger";
 import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
+import { MESSAGE } from "triple-beam";
 
-export const loggerLevels = winston.config.npm.levels;
+/**
+ * Creates a daily rotate file transport for the provided @level
+ * For example, if you pass warn as the level, then the file will only contain the warn logs
+ */
+const createDailyRotateFileTransportByLevel = (
+	level: LoggerLevels,
+	opts: DailyRotateFile.DailyRotateFileTransportOptions
+) => {
+	const transport = new DailyRotateFile({
+		//@ts-expect-error wrongly complains about the type
+		filename: "%DATE%.log",
+		datePattern: "MM-DD-YYYY",
+		json: true,
+		level,
+		dirname: `logs/${level}`,
+		maxSize: "20m",
+		...opts,
+	});
+	transport.log = (info, next = () => "") => {
+		//We only want to save those logs that match the level
+		if (!info.level.includes(level)) return;
+		transport.logStream.write(info[MESSAGE] + transport.options.eol);
+		transport.emit("logged", info);
+		//@ts-expect-error sometimes the next fn requires these params
+		next(null, true);
+	};
 
-const transport: DailyRotateFile = new DailyRotateFile({
-	filename: "%DATE%.log",
-	datePattern: "MM-DD-YYYY",
-	maxSize: "20m",
-	maxFiles: "14d",
-	json: true,
-	dirname: "logs/",
-});
+	return transport;
+};
+
+const transports: DailyRotateFile[] = [
+	createDailyRotateFileTransportByLevel("error", {
+		maxFiles: "14d",
+	}),
+	createDailyRotateFileTransportByLevel("warn", {
+		maxFiles: "10d",
+	}),
+	createDailyRotateFileTransportByLevel("info", {
+		maxFiles: "2d",
+	}),
+	new DailyRotateFile({
+		filename: "%DATE%.log",
+		datePattern: "MM-DD-YYYY",
+		json: true,
+		level: "silly",
+		dirname: "logs/all",
+		maxSize: "20m",
+		maxFiles: "1d",
+	}),
+];
 
 export const logger = winston.createLogger({
 	levels: loggerLevels,
+	level: "silly",
 	format: winston.format.combine(
 		winston.format.timestamp({
 			format: "YYYY-MM-DD HH:mm:ss",
@@ -24,7 +67,7 @@ export const logger = winston.createLogger({
 		winston.format.colorize(),
 		winston.format.json()
 	),
-	transports: ENV_VARS.NODE_ENV === "dev" ? [] : transport,
+	transports: ENV_VARS.NODE_ENV === "production" ? transports : [],
 	exceptionHandlers: [new winston.transports.File({ filename: "logs/exceptions.log" })],
 });
 
@@ -32,7 +75,7 @@ export const logger = winston.createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== "production") {
+if (ENV_VARS.NODE_ENV !== "production") {
 	logger.add(
 		new winston.transports.Console({
 			format: winston.format.simple(),
