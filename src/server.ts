651687946b4b791bb03960c697e71914302b0591
nicolau <marcosnicolau2003@gmail.com>
2023-01-31 19:14:17 -0300

    feat: ✨ start server
    created entry file for server and put all the loaders

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import { ENV_VARS } from "config/env";
 import express, { Application } from "express";
-import { getHelloWord } from "controllers";
+import { appLoaders } from "loaders";
+import { LoggerService } from "services";
 
-export const startServer = (app: Application) => {
-	const PORT = 5000;
-	app.get("/", getHelloWord);
-	app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+export const startServer = async (app: Application) => {
+	try {
+		const PORT = ENV_VARS.PORT || 5003;
+		await appLoaders(app);
+		app.listen(PORT, () => LoggerService.sendLog(`server ready and listening on port ${PORT}`));
+	} catch (err) {
+		console.error(`WARNING, COULD NOT START SERVER, err: ${err}`);
+	}
 };
 
 startServer(express());
