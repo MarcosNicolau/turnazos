2ae8041a22b263e9d8adec46cae8a8b3332af855
nicolau <marcosnicolau2003@gmail.com>
2023-03-23 18:45:54 -0300

    refactor: ♻️ removed migrations
    removed all migration, because we are still working with the models

--- business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/prisma/migrations/20230323123325_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[id,user_id]` on the table `Business` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Business_id_user_id_key" ON "Business"("id", "user_id");
