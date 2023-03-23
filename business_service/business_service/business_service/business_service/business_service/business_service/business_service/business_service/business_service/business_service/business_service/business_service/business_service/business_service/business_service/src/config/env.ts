0ba76a46a2f508c08be342f63ba2fbb789f0d047
nicolau <marcosnicolau2003@gmail.com>
2023-03-23 09:17:14 -0300

    feat(services): ✨ navigation
    created navigation service with geocoding and search

--- business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/src/config/env.ts
+++ business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/business_service/src/config/env.ts
@@ -14,4 +14,5 @@ export const ENV_VARS = {
 	REDIS_PORT: process.env.REDIS_PORT,
 	RABBIT_MQ_URL: process.env.RABBIT_MQ_URL,
 	NODE_ENV: process.env.NODE_ENV || "dev",
+	NAVIGATION_SERVICE_URL: process.env.NAVIGATION_SERVICE_URL,
 } as const;
