d5e4d054b684af5d69c258aa18dd864f0a6a4d3f
nicolau <marcosnicolau2003@gmail.com>
2023-08-03 14:24:58 -0300

    logger_service

--- merge_repo.sh
+++ merge_repo.sh
@@ -1,7 +1,8 @@
 cd ../back_end/$1
 git filter-repo --to-subdirectory-filter $1
 
-cd ../../migration 
+cd ../../migration
+cd $1
 git remote add $1 ../back_end/$1 
 git fetch $1 
 git merge --allow-unrelated-histories $1/staging # or whichever branch you want to merge
