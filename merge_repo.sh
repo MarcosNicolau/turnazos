6511caa206c7b05e92982d8b07bbd3ee43674704
nicolau <marcosnicolau2003@gmail.com>
2023-08-03 14:21:36 -0300

    auth_service

---
+++ merge_repo.sh
@@ -0,0 +1,8 @@
+cd ../back_end/$1
+git filter-repo --to-subdirectory-filter $1
+
+cd ../../migration 
+git remote add $1 ../back_end/$1 
+git fetch $1 
+git merge --allow-unrelated-histories $1/staging # or whichever branch you want to merge
+git remote remove $1
